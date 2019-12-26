0319494c850beae07a5e8c43f27b2d1bed35e48b
Evan Carr <ecarr@optaros.com>
2019-12-25 21:45:11 -0600

    Added a shuffle and deal feature to the deck class. Also added a .gitignore file to ignore the __pycache__ folder. Merry Christmasgit status

--- src/Deck.py
+++ src/Deck.py
@@ -1,15 +1,18 @@
+import random
+
 from Card import Card
 
 class Deck:
 	def __init__(self):
 		self.cards = []
-		numbers = [2, 3, 10, 11, 15]
-		self.cards = [card for card in (Card(number) for number in numbers) if card.valid]
-
-		# for i in range(2, 98):
-		# 	self.deck.append(Card(i))
+		self.cards = [card for card in (Card(number) for number in range(2, 98)) if card.valid]
 
 	def __str__(self):
 		return self.deck[0]
 
+	def shuffle(self):
+		random.shuffle(self.cards, random.random)
+
+	def deal(self):
+		return self.cards.pop(0)
 
--- src/main.py
+++ src/main.py
@@ -4,16 +4,8 @@ from Deck import Deck
 print("Starting Card Test")
 
 c = Card(84)
+print(c.value)
 
 d = Deck()
-
-print(c.value)
-# print(c.factors)
-
-
-# print(type(d.cards))
-[print(i) for i in d.cards]
-
-# print(d.deck)
-# print(len(d.deck))
-# print(c)
+d.shuffle()
+print(d.deal().value)
